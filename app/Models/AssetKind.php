1288b5762b9f595678fedb12e430fdcbafd9a390
Rachel <rachet2002@hotmail.com>
2025-10-01 23:25:36 +0700

    seeder kind

--- app/Models/AssetKind.php
+++ app/Models/AssetKind.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssetKind extends Model
 {
-    //
+    protected $fillable = [        
+        'asset_category_id',
+        'asset_code',
+        'type_name',
+        'useful_life',
+        'depreciation_rate'
+    ];
 }
--- database/migrations/2025_09_28_144958_create_asset_kinds_table.php
+++ database/migrations/2025_09_28_144958_create_asset_kinds_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('asset_kinds', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('asset_categories')->cascadeOnDelete();
-            $table->string('code', 50)->nullable();           // รหัสครุภัณฑ์
-            $table->string('name');                           // ชื่อชนิดครุภัณฑ์
+            $table->foreignId('asset_category_id')->constrained('asset_categories')->cascadeOnDelete();
+            $table->string('asset_code', 50)->nullable();           // รหัสครุภัณฑ์
+            $table->string('type_name');                           // ชื่อชนิดครุภัณฑ์
             $table->unsignedSmallInteger('useful_life')->nullable(); // อายุการใช้งาน (ปี)
             $table->decimal('depreciation_rate', 5, 2)->nullable();  // ค่าเสื่อม (% ต่อปี)
             $table->timestamps();
